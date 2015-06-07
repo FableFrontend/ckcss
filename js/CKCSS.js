4cd3a84dd8b8fa36885d05de42306bb3ad743fe7
CheckZhang <306814290@qq.com>
2015-06-08 01:16:04 +0800

    Fit Justify Layout in IE 8-11;

--- js/CKCSS.js
+++ js/CKCSS.js
@@ -1,6 +1,7 @@
 $(function(){
     // if IE, adjust Justify Layout table-cell height
-    if(!+[1,]){
+    //if(!+[1,]){
+    if (!!window.ActiveXObject || "ActiveXObject" in window){
         $(".justifies").resize(function(e){
                 $(this).children("div").children(".jf-bg").height($(this).height());
          }).resize();
